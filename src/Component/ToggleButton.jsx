239a6cf2ae393ef38f1e80e1d6eb2739f4adafff
asif <asiftahashildar17@gmail.com>
2022-12-08 16:55:40 +0530

    css-emoji

--- src/Component/ToggleButton.jsx
+++ src/Component/ToggleButton.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import "./togglebutton.css";
+import TagFacesIcon from "@mui/icons-material/TagFaces";
+import SentimentVeryDissatisfiedIcon from "@mui/icons-material/SentimentVeryDissatisfied";
 
 export const ToggleButton = () => {
   const [clickChange, setClickChange] = useState(false);
@@ -21,9 +23,27 @@ export const ToggleButton = () => {
         ></div>
       </div>
       {clickChange ? (
-        <h1 className={clickChange ? "text-change" : "text"}>Bulb Off</h1>
+        <>
+          <h1 className={clickChange ? "text-change" : "text"}>Bulb Off</h1>
+          <SentimentVeryDissatisfiedIcon
+            style={{
+              width: "100px",
+              height: "100px",
+              color: "rgb(148, 148, 10)",
+            }}
+          />
+        </>
       ) : (
-        <h1>Bulb On</h1>
+        <>
+          <h1>Bulb On</h1>
+          <TagFacesIcon
+            style={{
+              width: "100px",
+              height: "100px",
+              color: "rgb(150, 150, 20)",
+            }}
+          />
+        </>
       )}
       <div className={clickChange ? "name-text-change" : "name-text"}>
         <h1>A</h1>
